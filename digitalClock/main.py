5898ce78fe4b2f5139e0980345524471c985f255
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-10-22 10:49:10 +0700

    Adding

---
+++ digitalClock/main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from time import strftime
+
+root = Tk()
+root.title("Digital Clock")
+
+
+def time():
+    string = strftime("%H:%M:%S %p")
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
+
+lbl = Label(root, font=("arial", 160, "bold"), bg="black", fg="white")
+
+lbl.pack(anchor="center", fill="both", expand=1)
+
+time()
+mainloop()
